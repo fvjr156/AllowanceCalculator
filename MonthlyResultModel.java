fb88494bc929cb79c5e23ab72d8acefd51768f06
fvjr156 <juniorvillanueva156@gmail.com>
2025-02-15 16:00:40 +0800

    getters and setters, explanation document

--- MonthlyResultModel.java
+++ MonthlyResultModel.java
@@ -2,16 +2,41 @@ public class MonthlyResultModel {
     private int __months;
     private double __daily_allowance;
     private double __total_allowance;
+
+    public int get__months() {
+        return this.__months;
+    }
+
+    public void set__months(int __months) {
+        this.__months = __months;
+    }
+
+    public double get__daily_allowance() {
+        return this.__daily_allowance;
+    }
+
+    public void set__daily_allowance(double __daily_allowance) {
+        this.__daily_allowance = __daily_allowance;
+    }
+
+    public double get__total_allowance() {
+        return this.__total_allowance;
+    }
+
+    public void set__total_allowance(double __total_allowance) {
+        this.__total_allowance = __total_allowance;
+    }
+
     public MonthlyResultModel(int months, double daily_allowance, double total_allowance) {
         this.__daily_allowance = daily_allowance;
         this.__months = months;
         this.__total_allowance = total_allowance;
     }
+
     public void printResult() {
         System.out.printf("\nYour daily allowance is: %.2f.\nYour total allowance for %d months is: %.2f\n",
-            __daily_allowance,
-            __months, 
-            __total_allowance
-        );
+                __daily_allowance,
+                __months,
+                __total_allowance);
     }
 }
--- SemestralResultModel.java
+++ SemestralResultModel.java
@@ -3,6 +3,39 @@ public class SemestralResultModel {
     private double __daily_allowance;
     private double __semestral_allowance;
     private double __total_allowance;
+
+    public int get__semester() {
+        return this.__semester;
+    }
+
+    public void set__semester(int __semester) {
+        this.__semester = __semester;
+    }
+
+    public double get__daily_allowance() {
+        return this.__daily_allowance;
+    }
+
+    public void set__daily_allowance(double __daily_allowance) {
+        this.__daily_allowance = __daily_allowance;
+    }
+
+    public double get__semestral_allowance() {
+        return this.__semestral_allowance;
+    }
+
+    public void set__semestral_allowance(double __semestral_allowance) {
+        this.__semestral_allowance = __semestral_allowance;
+    }
+
+    public double get__total_allowance() {
+        return this.__total_allowance;
+    }
+
+    public void set__total_allowance(double __total_allowance) {
+        this.__total_allowance = __total_allowance;
+    }
+
     public SemestralResultModel(int xsemester, double xdailyallowance, double xsemestralallowance, double xtotalallowance) {
         this.__semester = xsemester;
         this.__daily_allowance = xdailyallowance;
